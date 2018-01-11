ec601874bd9ab9fcc849d5e729bb6cadf42cb3ca
Trever Andersen <trever.r.andersen@gmail.com>
2018-01-11 00:09:39 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,12 +3,18 @@ require 'sinatra/base'
 class App < Sinatra::Base
 
   get '/newteam' do
-    
+
     erb :newteam
   end
 
-  post '/team' do 
-    
+  post '/team' do
+    "#{params[:teamName]}"
+    "#{params[:coach]}"
+    "#{params[:pointGuard]}"
+    "#{params[:shootingGuard]}"
+    "#{params[:smallForward]}"
+    "#{params[:powerForward]}"
+    "#{params[:center]}"
   end
 
 end
